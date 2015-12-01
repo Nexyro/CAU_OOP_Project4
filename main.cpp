ccbfb9a8330604ae8126c0de86e792fa3ba85d5a
Rodrigue <rodrigue.developer@gmail.com>
2015-12-01 23:38:41 +0900

    replace sqlite3 c files by lib and dll files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include "SQLite3.h"
+
+#include "inc\sqlite3.h"
 
 using namespace std;
-
-/*Here you can find examples on how to do create table, insert into table and select on our database*/
 
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -28,7 +27,7 @@ int main(int argc, char* argv[])
 	char *insertIntoMovies;
 	char *insertIntoUsers;
 	const char* data = "Callback function called";
-	
+
 
 	/*-----------------Open Database------------------*/
 	rc = sqlite3_open("movies.db", &db);
@@ -52,80 +51,77 @@ int main(int argc, char* argv[])
 	else {
 		fprintf(stdout, "Operation done successfully\n");
 	}
-	
+
 
 
 	/*-------------------INSERT INTO-------------*/
 
 	/*insertIntoUsers = "INSERT INTO USERS ( USERNAME, PASSWORD)" \
-		"VALUES ('ttammero','12345678');";
-	
-	//WARNING! Do not use apostrophes in the description. SQLite will understand them as string ending limiters.
+	"VALUES ('ttammero','12345678');";
 
+	//WARNING! Do not use apostrophes in the description. SQLite will understand them as string ending limiters.
 	insertIntoMovies = "INSERT INTO MOVIES (NAME, SHOW_TIME, THEATER_NAME, PREMIERE, AGELIMIT, DURATION_IN_MINUTES, STARS, DESCRIPTION)"	\
-		"VALUES('007 SPECTRE', '2015-11-29 20:00', 'Some movie theater', '2015-10-30', 12, 149, 5, 'A cryptic message from Bonds past sends him on a trail to uncover a sinister organization. While M battles political forces to keep the secret service alive, Bond peels back the layers of deceit to reveal the terrible truth behind SPECTRE.');";
-		
+	"VALUES('007 SPECTRE', '2015-11-29 20:00', 'Some movie theater', '2015-10-30', 12, 149, 5, 'A cryptic message from Bonds past sends him on a trail to uncover a sinister organization. While M battles political forces to keep the secret service alive, Bond peels back the layers of deceit to reveal the terrible truth behind SPECTRE.');";
+
 	/* Execute SQL statement */
 	/*
 	rc = sqlite3_exec(db, insertIntoUsers, callback, 0, &zErrMsg);
 	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
+	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	sqlite3_free(zErrMsg);
 	}
 	else {
-		fprintf(stdout, "Records created successfully\n");
+	fprintf(stdout, "Records created successfully\n");
 	}
 
-	
 	rc = sqlite3_exec(db, insertIntoMovies, callback, 0, &zErrMsg);
 	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
+	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	sqlite3_free(zErrMsg);
 	}
 	else {
-		fprintf(stdout, "Records created successfully\n");
+	fprintf(stdout, "Records created successfully\n");
 	}*/
-	
+
 
 
 
 	//CREATE TABLES
-	
+
 
 	/*---------Create SQL Statements-----------*/
 	/*createUsers="CREATE TABLE USERS("	\
-		"USERNAME VARCHAR(16) PRIMARY KEY NOT NULL,"	\
-		"PASSWORD VARCHAR(32) NOT NULL);";
-		
-	createMovies = "CREATE TABLE MOVIES("	\
-		"ID INTEGER PRIMARY KEY NOT NULL,"
-		"NAME VARCHAR(64) NOT NULL,"	\
-		"SHOW_TIME DATETIME NOT NULL,"	\
-		"THEATER_NAME VARCHAR(64) NOT NULL,"	\
-		"PREMIERE DATE,"	\
-		"AGELIMIT SMALLINT,"	\
-		"DURATION_IN_MINUTES SMALLINT,"	\
-		"STARS SMALLINT,"	\
-		"DESCRIPTION TEXT);";
+	"USERNAME VARCHAR(16) PRIMARY KEY NOT NULL,"	\
+	"PASSWORD VARCHAR(32) NOT NULL);";
 
+	createMovies = "CREATE TABLE MOVIES("	\
+	"ID INTEGER PRIMARY KEY NOT NULL,"
+	"NAME VARCHAR(64) NOT NULL,"	\
+	"SHOW_TIME DATETIME NOT NULL,"	\
+	"THEATER_NAME VARCHAR(64) NOT NULL,"	\
+	"PREMIERE DATE,"	\
+	"AGELIMIT SMALLINT,"	\
+	"DURATION_IN_MINUTES SMALLINT,"	\
+	"STARS SMALLINT,"	\
+	"DESCRIPTION TEXT);";
 	/*---------Execute SQL Statements------------*/
 
 	/*rc = sqlite3_exec(db, createUsers, callback, 0, &zErrMsg);
 	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
+	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	sqlite3_free(zErrMsg);
 	}
 	else {
-		fprintf(stdout, "Table created successfully\n");
+	fprintf(stdout, "Table created successfully\n");
 	}
-	
+
 	rc = sqlite3_exec(db, createMovies, callback, 0, &zErrMsg);
 	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
+	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	sqlite3_free(zErrMsg);
 	}
 	else {
-		fprintf(stdout, "Table created successfully \n");
+	fprintf(stdout, "Table created successfully \n");
 	}
 	*/
 
@@ -134,4 +130,4 @@ int main(int argc, char* argv[])
 	return 0;
 
 
-	}
+}
